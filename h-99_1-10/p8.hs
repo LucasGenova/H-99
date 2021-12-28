40b8a4bcd725a6c576feb4f446fbc4519ce000c9
Lucas Genova <glucasemanuel@gmail.com>
2021-12-28 13:00:20 -0300

    add solution for problem 8

---
+++ h-99_1-10/p8.hs
@@ -0,0 +1,5 @@
+--(**) Eliminate consecutive duplicates of list elements. If a list contains repeated elements they should be replaced with a single copy of the element. The order of the elements should not be changed.
+
+compress :: (Eq a) => [a] -> [a]
+compress (x:xs@(y:ys)) = if x == y then compress xs else x:compress xs
+compress x = x
