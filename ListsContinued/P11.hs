0e28b1d6710a2e02862c945759db1cb70d7227a2
Lucas Genova <glucasemanuel@gmail.com>
2021-12-29 01:53:29 -0300

    add solution for problem 12 (and necessary dependences)

--- ListsContinued/P11.hs
+++ ListsContinued/P11.hs
@@ -1,4 +1,4 @@
-module H99.ListsContinued.P11 (encodeModified) where
+module H99.ListsContinued.P11 (encodeModified, Encodings (..)) where
 import Data.List ( group )
 
 --(*) Modified run-length encoding. Modify the result of problem 10 in such a way that if an element has no duplicates it is simply copied into the result list. Only elements with duplicates are transferred as (N E) lists.
---
+++ ListsContinued/P12.hs
@@ -0,0 +1,9 @@
+module H99.ListsContinued.P12 where
+import H99.ListsContinued.P11 (Encodings (..), encodeModified)
+
+--(**) Decode a run-length encoded list. Given a run-length code list generated as specified in problem 11. Construct its uncompressed version.
+
+decodeModified :: [Encodings a] -> [a]
+decodeModified ((Multiple n a):es) = replicate n a ++ decodeModified es
+decodeModified ((Single a): es) = a : decodeModified es
+decodeModified _ = []
